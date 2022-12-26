d9d17eacb981b76ffeaeb612a53a5f1241c386d4
Jim Jiang <jim@lotlab.org>
2022-12-26 12:39:18 +0800

    ci: remove dependency, tweak build sequence

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,9 @@ import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: true
+  },
   plugins: [
     wasmPack('../mji-craftwork'),
     vue({
