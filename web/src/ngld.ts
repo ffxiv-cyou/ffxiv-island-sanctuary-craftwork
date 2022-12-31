cfb0910b2aefa7f186ce0e2a810cfc465d7a0332
Jim Jiang <jim@lotlab.org>
2022-12-31 16:29:56 +0800

    feat: 改进悬浮窗的显示

--- web/src/ngld.ts
+++ web/src/ngld.ts
@@ -1,26 +1,19 @@
 import "./ngld/common.js";
 
 let element = document.getElementById('token');
-
-// 添加数据处理
-addOverlayListener('onMJICraftworkData', (data) => {
-  console.log(`MJI Craftwork DATA: ${JSON.stringify(data)}`);
-  setContent(data.data);
-});
-
-// 注册完毕，启动悬浮窗事件
-startOverlayEvents();
-
 let clostBtn = document.getElementById('close');
 if (clostBtn != null) {
   clostBtn.onclick = () => {
-    if (element != null) {
-      element.innerText = "";
-    }
+    toggleText();
+    return false;
   };
 }
 
 let basePath = document.location.protocol + "//" + document.location.host;
+let link = document.getElementById('link');
+if (link != null) {
+  link.setAttribute('href', basePath);
+}
 
 function setContent(content: string) {
   if (element != null) {
@@ -29,7 +22,52 @@ function setContent(content: string) {
   }
 }
 
-let link = document.getElementById('link');
-if (link != null) {
-  link.setAttribute('href', basePath);
+function onZoneChange(zone: number) {
+  if (zone == 1055) {
+    showText();
+  } else {
+    hideText();
+  }
 }
+
+function hideText() {
+  let container = document.getElementById("all");
+  if (container != null) {
+    container.classList.add("hide");
+  }
+}
+
+function showText() {
+  let container = document.getElementById("all");
+  if (container != null) {
+    container.classList.remove("hide");
+  }
+}
+
+function toggleText() {
+  let container = document.getElementById("all");
+  if (container != null) {
+    container.classList.toggle("hide");
+  }
+}
+
+// 添加数据处理
+addOverlayListener('onMJICraftworkData', (data) => {
+  setContent(data.data);
+});
+
+addOverlayListener('onMJIZoneChanged', (data) => {
+  onZoneChange(data.zoneID);
+});
+
+// 注册完毕，启动悬浮窗事件
+startOverlayEvents();
+
+callOverlayHandler({ call: "RequestMJIZoneState" }).then((val) => {
+  let ele = document.getElementById("not-inited");
+  if (ele != null) {
+    ele.classList.add("hide");
+  }
+  showText();
+  onZoneChange(val.zoneID);
+});
--- web/src/ngld/common.d.ts
+++ web/src/ngld/common.d.ts
@@ -1,6 +1,6 @@
 declare function dispatchOverlayEvent(msg: any);
 declare function addOverlayListener(event: string, cb: (msg: any) => void);
 declare function removeOverlayListener(event: string, cb: (msg: any) => void);
-declare function callOverlayHandler(msg: any);
+declare function callOverlayHandler(msg: any): Promise<any>;
 
 declare function startOverlayEvents();
