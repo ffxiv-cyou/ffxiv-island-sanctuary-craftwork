0b192a68ab1a33d2ae5e630aeabeb688f979450e
Jim Jiang <jim@lotlab.org>
2023-01-01 11:47:43 +0800

    feat: 添加统计代码

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
+import VueGtag from "vue-gtag";
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(VueGtag, { config: { id: "G-55CXMHCLVY" } });
 app.mount('#app')
