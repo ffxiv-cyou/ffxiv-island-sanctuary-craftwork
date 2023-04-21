7bedf563e11ff696a589ce66b76055ed76e7b2c1
Jim Jiang <jim@lotlab.org>
2023-04-21 20:02:59 +0800

    test: 抽出测试公共逻辑

--- mji-craftwork/tests/data.rs
+++ mji-craftwork/tests/data.rs
@@ -1,3 +1,5 @@
+use mji_craftwork::data::{Recipe, Popularity, GameDataRepo};
+
 pub const CRAFT_OBJECTS: &'static [[u16; 16]] = &[
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [1, 37628, 8, 0, 0, 0, 2, 7, 2, 0, 0, 0, 0, 1, 4, 28],
@@ -572,3 +574,42 @@ pub const DEMAND_LIST: &'static [u8] = &[
     0x00, 0x11, 0x24, 0x22, 0x24, 0x24, 0x11, 0x00, 0x22, 0x22, 0x10, 0x11, 0x24, 0x24, 0x24, 0x24,
     0x11, 0x11, 0x24, 0x11, 0x24, 0x32, 0x32, 0x32, 0x32, 0x32, 0x32, 0x32, 0x32, 0x32, 0x32, 0x32,
 ];
+
+/// 获取配方列表
+pub fn recipes() -> Vec<Recipe> {
+    let mut recpies = vec![];
+    for i in 0..CRAFT_OBJECTS.len() {
+        let rec = Recipe {
+            id: CRAFT_OBJECTS[i][0],
+            theme1: CRAFT_OBJECTS[i][2] as u8,
+            theme2: CRAFT_OBJECTS[i][3] as u8,
+            level: CRAFT_OBJECTS[i][13] as u8,
+            craft_time: CRAFT_OBJECTS[i][14] as u8,
+            value: CRAFT_OBJECTS[i][15],
+            cost: 0,
+        };
+        recpies.push(rec);
+    }
+    recpies
+}
+
+/// 获取欢迎度列表
+pub fn popularity() -> Vec<Vec<Popularity>> {
+    let mut pop_vec = vec![vec![]];
+    for r in POPULARITY_LIST {
+        let mut v = vec![];
+        for i in 1..r.len() {
+            v.push(Popularity::from(r[i]));
+        }
+        pop_vec.push(v)
+    }
+    pop_vec
+}
+
+pub fn new_repo(pop: usize) -> GameDataRepo {
+    let recpies = recipes();
+    let pop_vec = popularity();
+    let mut repo = GameDataRepo::new(recpies, pop_vec);
+    repo.set_popular_pattern(pop);
+    repo
+}
--- mji-craftwork/tests/solver.rs
+++ mji-craftwork/tests/solver.rs
@@ -1,48 +1,17 @@
 use mji_craftwork::{
-    data::{CraftworkInfo, GameDataRepo, Popularity, Recipe},
+    data::{CraftworkInfo},
     init_repo, simulate, solve_singleday,
     solver::{BFSolver, SolveLimit, Solver},
 };
 
-mod test_data;
-use test_data::{CRAFT_OBJECTS, DEMAND_LIST, POPULARITY_LIST};
+mod data;
+use crate::data::new_repo;
 
-fn load_data<const T: usize>() -> GameDataRepo {
-    let mut recpies = vec![];
-
-    for i in 0..usize::min(CRAFT_OBJECTS.len(), T) {
-        let rec = Recipe {
-            id: CRAFT_OBJECTS[i][0],
-            theme1: CRAFT_OBJECTS[i][2] as u8,
-            theme2: CRAFT_OBJECTS[i][3] as u8,
-            level: CRAFT_OBJECTS[i][13] as u8,
-            craft_time: CRAFT_OBJECTS[i][14] as u8,
-            value: CRAFT_OBJECTS[i][15],
-            cost: 0,
-        };
-        recpies.push(rec);
-    }
-
-    let mut pop_vec = vec![];
-    for r in POPULARITY_LIST {
-        let mut pop: Vec<Popularity> = vec![];
-        for i in 1..usize::min(r.len(), T + 1) {
-            pop.push(r[i].into());
-        }
-        pop_vec.push(pop);
-    }
-    let mut repo = GameDataRepo::new(recpies, pop_vec);
-
-    let current_pop = DEMAND_LIST[0];
-    repo.set_popular_pattern(current_pop as usize);
-    // let next_pop = DEMAND_LIST[1];
-
-    repo
-}
-
-fn load_data_init() -> GameDataRepo {
+#[test]
+fn init_test() {
+    use data::{ CRAFT_OBJECTS, POPULARITY_LIST};
     let mut recipes = vec![];
-
+  
     for i in 0..CRAFT_OBJECTS.len() {
         recipes.push(CRAFT_OBJECTS[i][0] as u16);
         recipes.push(CRAFT_OBJECTS[i][2] as u16);
@@ -50,6 +19,7 @@ fn load_data_init() -> GameDataRepo {
         recipes.push(CRAFT_OBJECTS[i][13] as u16);
         recipes.push(CRAFT_OBJECTS[i][14] as u16);
         recipes.push(CRAFT_OBJECTS[i][15] as u16);
+        recipes.push(0);
     }
     let mut pop_vec = vec![];
     for r in POPULARITY_LIST {
@@ -57,18 +27,9 @@ fn load_data_init() -> GameDataRepo {
             pop_vec.push(r[i] as u8);
         }
     }
-
-    init_repo(recipes, pop_vec, 62)
-}
-
-fn load_test_demand() -> Vec<i8> {
-    vec![9; 62]
-}
-
-#[test]
-fn init_test() {
-    let repo = load_data_init();
-    let demands = load_test_demand();
+  
+    let repo = init_repo(recipes, pop_vec, 62);
+    let demands = vec![9; 62];
 
     solve_singleday(
         &repo,
@@ -83,8 +44,8 @@ fn init_test() {
 
 #[test]
 fn predict() {
-    let repo = load_data::<51>();
-    let demands = load_test_demand();
+    let repo = new_repo(1);
+    let demands = vec![9; 62];
     let solver = BFSolver::new(&repo, CraftworkInfo::new(0, 35, 1, 1));
     let empty = vec![];
     let limit = SolveLimit::new(10, &empty, 24, false);
@@ -103,11 +64,11 @@ fn predict() {
 
 #[test]
 fn simulator() {
-    let repo = load_data_init();
-    let demands = load_test_demand();
+    let repo = new_repo(1);
+    let demands = vec![9; 62];
 
     let state = CraftworkInfo::new(0, 35, 1, 1);
     let arr = vec![13, 23, 13, 23];
     let result = simulate(&repo, &state, arr, &demands);
-    assert_eq!(result, [38, 240, 78, 246, 0, 0]);
+    assert_eq!(result, [0, 38, 240, 78, 246, 0, 0]);
 }
